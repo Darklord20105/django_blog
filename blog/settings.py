fc8259b3c05683298f8b140b131c3985afcc2649
unknown <omarhj1992@gmail.com>
2020-04-18 19:32:18 +0300

    fix12

--- blog/settings.py
+++ blog/settings.py
@@ -163,13 +163,12 @@ USE_TZ = True
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SITE_ID = 1
 
